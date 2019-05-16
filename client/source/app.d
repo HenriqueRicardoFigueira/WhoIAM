50dc69a77e636dbf2166412ca7667fa56610b020
Henrique Figueira <39686167+HenriqueRicardoFigueira@users.noreply.github.com>
2019-05-16 17:15:23 -0300

    create player and game

--- client/source/app.d
+++ client/source/app.d
@@ -1,15 +1,63 @@
-module app;import std.socket, std.stdio;
+module app;
+import std.socket, std.stdio;
+
+final class Player
+{
+	string name;
+	int score;
+	bool master;
+	bool token;
+
+	this(string name)
+	{
+		this.name = name;
+        this.score = 0;
+        this.token = false;
+	}
+    
+    string getName()
+    {
+        return name;
+    }
+
+	void setToken(bool tok)
+	{
+		this.token = tok;
+	}
+
+    bool getToken(){
+        return token;
+    }
+
+	void setMaster(bool xxx)
+	{
+		this.master = xxx;
+	}
+
+    bool getMaster()
+    {
+        return master;    
+    }
+
+	void setScore()
+	{
+		this.score += 10;
+	}
+}
+
 void main() {
-  
+    char[] name;
     auto socket = new Socket(AddressFamily.INET,  SocketType.STREAM);
     char[1024] buffer;
     socket.connect(new InternetAddress("localhost", 2525));
     auto received = socket.receive(buffer); // wait for the server to say hello
     writeln("Server said: ", buffer[0 .. received]);
-    socket.send("noix na fita vagabundo");
+    writeln("Digite seu nick: ");
+    readln(name);
+    socket.send(name);
+    Player player = new Player(cast(string)name);
     foreach(line; stdin.byLine) {
        socket.send(line);
        writeln("Server said: ", buffer[0 .. socket.receive(buffer)]);
-       
     }
 }
--- server/source/app.d
+++ server/source/app.d
@@ -1,6 +1,131 @@
-module app;void main() {
-   import std.socket; 
-   import std.stdio;
+module app;
+import std.socket;
+import std.stdio;
+
+final class Player
+{
+   string name;
+   string ip;
+   int score;
+   bool master;
+   bool token;
+
+   this(string name)
+   {
+      this.name = name;
+      this.score = 0;
+      this.token = false;
+   }
+
+   string getName()
+   {
+      return name;
+   }
+
+   void setToken(bool tok)
+   {
+      this.token = tok;
+   }
+
+   bool getToken()
+   {
+      return token;
+   }
+
+   void setMaster(bool xxx)
+   {
+      this.master = xxx;
+   }
+
+   bool getMaster()
+   {
+      return master;
+   }
+
+   void setScore()
+   {
+      this.score += 10;
+   }
+
+   string getIp()
+   {
+      return ip;
+   }
+   
+   void setIp(string ip)
+   {
+      this.ip = ip;
+   }
+}
+
+final class Game
+{
+   int rodada;
+   Player[] players;
+   string playerDaVez;
+   string master;
+   string resposta;
+
+   this()
+   {
+      this.rodada = 0;
+   }
+
+   int getRodada()
+   {
+      return rodada;
+   }
+
+   void setRodada()
+   {
+      this.rodada += 1;
+   }
+
+   Player[] getPlayers()
+   {
+      return players;
+   }
+
+   void setPlayer(Player id)
+   {
+      this.players ~= id;
+   }
+
+   string getMaster()
+   {
+      return master;
+   }
+
+   void setMaster(string name)
+   {
+      this.master = name;
+   }
+
+   string getResposta()
+   {
+      return resposta;
+   }
+
+   void setResposta(string resp)
+   {
+      this.resposta = resp;
+   }
+
+   string getPlayersDaVez()
+   {
+      return playerDaVez;
+   }
+
+   void setPlayerDaVez(string name)
+   {
+      this.playerDaVez = name;
+   }
+
+}
+
+void main()
+{
+
    auto listener = new Socket(AddressFamily.INET, SocketType.STREAM);
    listener.bind(new InternetAddress("localhost", 2525));
    listener.listen(10);
@@ -8,27 +133,50 @@ module app;void main() {
    Socket[] connectedClients;
    char[1024] buffer;
    bool isRunning = true;
-   while(isRunning) {
-       readSet.reset();
-       readSet.add(listener);
-       foreach(client; connectedClients) readSet.add(client);
-       if(Socket.select(readSet, null, null)) {
-          foreach(client; connectedClients)
-            if(readSet.isSet(client)) {
-                // read from it and echo it back
-                auto got = client.receive(buffer);
-                client.send(buffer[0 .. got]);
+   Game game = new Game();
+   while (isRunning)
+   {
+      readSet.reset();
+      readSet.add(listener);
+      foreach (client; connectedClients)
+         readSet.add(client);
+      if (Socket.select(readSet, null, null))
+      {
+         foreach (client; connectedClients)
+            if (readSet.isSet(client))
+            {
+               // read from it and echo it back
+               auto got = client.receive(buffer);
+               client.send(buffer[0 .. got]);
             }
-          if(readSet.isSet(listener)) {
-             // the listener is ready to read, that means
-             // a new client wants to connect. We accept it here.
-             auto newSocket = listener.accept();
-             newSocket.send("Hello!\n"); // say hello
-             connectedClients ~= newSocket; // add to our list
-             writeln("CLiente said:",  buffer[0 .. newSocket.receive(buffer)]);
-             
-          }
-       }
+         if (readSet.isSet(listener))
+         {
+            // the listener is ready to read, that means
+            // a new client wants to connect. We accept it here.
+            auto newSocket = listener.accept();
+            newSocket.send("Hello!\n"); // say hello
+            connectedClients ~= newSocket; // add to our list
+           
+            Player p1 = new Player(cast(string)buffer[0 .. newSocket.receive(buffer)]);
+            if (connectedClients.length == 1)
+            {
+               p1.setMaster(true);
+               p1.setIp(newSocket.hostName());
+               game.setPlayer(p1);
+               game.setMaster(p1.getName());
+            }
+            else{
+               game.setPlayer(p1);
+               p1.setIp(newSocket.remoteAddress().toAddrString());
+            }
+            Player[] x = game.getPlayers();
+            foreach(px; x)
+            {
+               writeln(px.getName());
+               writeln(px.getMaster());
+               writeln(px.getIp());
+            }
+         }
+      }
    }
 }
-
